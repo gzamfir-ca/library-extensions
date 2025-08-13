f8ba7c46222e7fad0482a21f3f2a866c82968fe1
gzamfir-ca <gzamfir_ca@icloud.com>
2025-08-13 19:30:14 -0400

    add values list map

---
+++ lib/src/main/java/libext/ValueListMap.java
@@ -0,0 +1,141 @@
+package libext;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class ValueListMap<K, V> implements Map<K, List<V>> {
+
+  private final Map<K, List<V>> map;
+
+  public ValueListMap() {
+    map = new HashMap<>();
+  }
+
+  public ValueListMap(Map<K, List<V>> listMap) {
+    map = listMap;
+  }
+
+  public Map<K, List<V>> getListMap() {
+    return map;
+  }
+
+  public List<V> putValue(K key, V element) {
+    map.computeIfAbsent(key, k -> new ArrayList<>()).add(element);
+    return map.get(key);
+  }
+
+  public List<V> removeValue(K key, V element) {
+    map.computeIfAbsent(key, k -> new ArrayList<>()).remove(element);
+    return map.get(key);
+  }
+
+  public Collection<V> valueList() {
+    Collection<V> valueList = new ArrayList<>();
+    for (List<V> list : map.values()) {
+      valueList.addAll(list);
+    }
+    return valueList;
+  }
+
+  public int valueCount() {
+    int count = 0;
+    for (List<V> list : map.values()) {
+      count += list.size();
+    }
+    return count;
+  }
+
+  @Override
+  public int size() {
+    return map.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return map.isEmpty();
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    return map.containsKey(key);
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    for (List<V> list : map.values()) {
+      for (V v : list) {
+        if (v.equals(value)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public List<V> get(Object key) {
+    return map.get(key);
+  }
+
+  @Override
+  public List<V> put(K key, List<V> value) {
+    map.computeIfAbsent(key, k -> new ArrayList<>()).addAll(value);
+    return map.get(key);
+  }
+
+  @Override
+  public List<V> remove(Object key) {
+    return map.remove(key);
+  }
+
+  @Override
+  public void putAll(Map<? extends K, ? extends List<V>> m) {
+    map.putAll(m);
+  }
+
+  @Override
+  public void clear() {
+    map.clear();
+  }
+
+  @Override
+  public Set<K> keySet() {
+    return map.keySet();
+  }
+
+  @Override
+  public Collection<List<V>> values() {
+    return map.values();
+  }
+
+  @Override
+  public Set<Entry<K, List<V>>> entrySet() {
+    return map.entrySet();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ValueListMap<?, ?> that = (ValueListMap<?, ?>) o;
+    return Objects.equals(map, that.map);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(map);
+  }
+
+  @Override
+  public String toString() {
+    return "MultipleValuesMap{" +
+        "map=" + map +
+        '}';
+  }
+}
---
+++ lib/src/test/java/libext/ValueListMapTest.java
@@ -0,0 +1,88 @@
+package libext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValueListMapTest {
+
+  @Test
+  void getListMap() {
+  }
+
+  @Test
+  void putValue() {
+  }
+
+  @Test
+  void removeValue() {
+  }
+
+  @Test
+  void valueList() {
+  }
+
+  @Test
+  void valueCount() {
+  }
+
+  @Test
+  void size() {
+  }
+
+  @Test
+  void isEmpty() {
+  }
+
+  @Test
+  void containsKey() {
+  }
+
+  @Test
+  void containsValue() {
+  }
+
+  @Test
+  void get() {
+  }
+
+  @Test
+  void put() {
+  }
+
+  @Test
+  void remove() {
+  }
+
+  @Test
+  void putAll() {
+  }
+
+  @Test
+  void clear() {
+  }
+
+  @Test
+  void keySet() {
+  }
+
+  @Test
+  void values() {
+  }
+
+  @Test
+  void entrySet() {
+  }
+
+  @Test
+  void testEquals() {
+  }
+
+  @Test
+  void testHashCode() {
+  }
+
+  @Test
+  void testToString() {
+  }
+}
