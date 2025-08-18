ea9659a288074dc7ae295907a87d2b0349b0a8c3
gzamfir-ca <gzamfir_ca@icloud.com>
2025-08-18 15:35:45 -0400

    extend list test

--- lib/src/test/java/libext/ListsTest.java
+++ lib/src/test/java/libext/ListsTest.java
@@ -7,20 +7,64 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Objects;
 import org.junit.jupiter.api.Test;
 
 class ListsTest {
 
+  static class CopyableRecord implements Copyable<CopyableRecord> {
+
+    int id;
+    String name;
+
+    public CopyableRecord(int id, String name) {
+      this.id = id;
+      this.name = name;
+    }
+
+    public CopyableRecord(CopyableRecord other) {
+      this.id = other.id;
+      this.name = other.name;
+    }
+
+    @Override
+    public CopyableRecord copy() {
+      return new CopyableRecord(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      CopyableRecord that = (CopyableRecord) o;
+      return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+      return "CopyableRecord [id=" + id + ", name=" + name + "]";
+    }
+  }
+
   @Test
   void newArrayListFromStream() {
     String s = """
-                            one   two three
-                                    four             five six seven
-                                                     eight nine              ten
-                                                                    \s""";
+        one   two three
+                four             five six seven
+                                 eight nine              ten
+                                                \s""";
     InputStream inputStream = new ByteArrayInputStream(s.getBytes());
-    try(BufferedReader bufferedReader = Readers.newBufferedReader(inputStream)) {
+    try (BufferedReader bufferedReader = Readers.newBufferedReader(inputStream)) {
       assertNotNull(bufferedReader);
       var input = Lists.newArrayList(bufferedReader);
       var expected = Arrays.asList("one", "two", "three", "four",
@@ -35,7 +79,7 @@ class ListsTest {
   void newArrayListFromPath() {
     Path path = Path.of("src/test/resources/readersTest.txt");
     assertNotNull(path);
-    try(BufferedReader bufferedReader = Readers.newBufferedReader(path)) {
+    try (BufferedReader bufferedReader = Readers.newBufferedReader(path)) {
       assertNotNull(bufferedReader);
       var input = Lists.newArrayList(bufferedReader);
       var expected = Arrays.asList("one", "two", "three", "four",
@@ -47,14 +91,30 @@ class ListsTest {
   }
 
   @Test
+  void newArrayListFromIterable() {
+    var original = new ArrayList<>(Arrays.asList(
+        new CopyableRecord(1, "one"),
+        new CopyableRecord(2, "two")
+    ));
+    var expected = new ArrayList<>(Arrays.asList(
+        new CopyableRecord(1, "one"),
+        new CopyableRecord(2, "two")
+    ));
+    var copy = Lists.newArrayList(original);
+    original.getFirst().id = 10;
+    original.getLast().name = "ten";
+    assertIterableEquals(expected, copy);
+  }
+
+  @Test
   void newLinkedListFromStream() {
     String s = """
-                            one   two three
-                                    four             five six seven
-                                                     eight nine              ten
-                                                                    \s""";
+        one   two three
+                four             five six seven
+                                 eight nine              ten
+                                                \s""";
     InputStream inputStream = new ByteArrayInputStream(s.getBytes());
-    try(BufferedReader bufferedReader = Readers.newBufferedReader(inputStream)) {
+    try (BufferedReader bufferedReader = Readers.newBufferedReader(inputStream)) {
       assertNotNull(bufferedReader);
       var input = Lists.newLinkedList(bufferedReader);
       var expected = Arrays.asList("one", "two", "three", "four",
@@ -69,7 +129,7 @@ class ListsTest {
   void newLinkedListFromPath() {
     Path path = Path.of("src/test/resources/readersTest.txt");
     assertNotNull(path);
-    try(BufferedReader bufferedReader = Readers.newBufferedReader(path)) {
+    try (BufferedReader bufferedReader = Readers.newBufferedReader(path)) {
       assertNotNull(bufferedReader);
       var input = Lists.newLinkedList(bufferedReader);
       var expected = Arrays.asList("one", "two", "three", "four",
@@ -81,14 +141,30 @@ class ListsTest {
   }
 
   @Test
+  void newLinkedListFromIterable() {
+    var original = new LinkedList<>(Arrays.asList(
+        new CopyableRecord(1, "one"),
+        new CopyableRecord(2, "two")
+    ));
+    var expected = new LinkedList<>(Arrays.asList(
+        new CopyableRecord(1, "one"),
+        new CopyableRecord(2, "two")
+    ));
+    var copy = Lists.newLinkedList(original);
+    original.getFirst().id = 10;
+    original.getLast().name = "ten";
+    assertIterableEquals(expected, copy);
+  }
+
+  @Test
   void newLinkedHashSetFromStream() {
     String s = """
-                            one   two three
-                                    four             five six seven
-                                                     eight nine              ten
-                                                                    \s""";
+        one   two three
+                four             five six seven
+                                 eight nine              ten
+                                                \s""";
     InputStream inputStream = new ByteArrayInputStream(s.getBytes());
-    try(BufferedReader bufferedReader = Readers.newBufferedReader(inputStream)) {
+    try (BufferedReader bufferedReader = Readers.newBufferedReader(inputStream)) {
       assertNotNull(bufferedReader);
       var input = Lists.newLinkedHashSet(bufferedReader);
       var expected = Arrays.asList("one", "two", "three", "four",
@@ -103,7 +179,7 @@ class ListsTest {
   void newLinkedHashSetFromPath() {
     Path path = Path.of("src/test/resources/readersTest.txt");
     assertNotNull(path);
-    try(BufferedReader bufferedReader = Readers.newBufferedReader(path)) {
+    try (BufferedReader bufferedReader = Readers.newBufferedReader(path)) {
       assertNotNull(bufferedReader);
       var input = Lists.newLinkedHashSet(bufferedReader);
       var expected = Arrays.asList("one", "two", "three", "four",
@@ -112,5 +188,21 @@ class ListsTest {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  @Test
+  void newLinkedHashSetFromIterable() {
+    var original = new LinkedHashSet<>(Arrays.asList(
+        new CopyableRecord(1, "one"),
+        new CopyableRecord(2, "two")
+    ));
+    var expected = new LinkedHashSet<>(Arrays.asList(
+        new CopyableRecord(1, "one"),
+        new CopyableRecord(2, "two")
+    ));
+    var copy = Lists.newLinkedHashSet(original);
+    original.getFirst().id = 10;
+    original.getLast().name = "ten";
+    assertIterableEquals(expected, copy);
   }
 }
